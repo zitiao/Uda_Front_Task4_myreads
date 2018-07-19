adc8956709e7549f5d7ac7f631d02f2bd3e26b19
Unknown <xiongyong178@126.com>
2018-07-19 12:26:15 +0800

    20180719
    fix：添加模板字面量

--- src/Components/BooksGrid.js
+++ src/Components/BooksGrid.js
@@ -2,20 +2,6 @@ import React from 'react'
 import '../App.css'
 import BookMoveSelect from './BookMoveSelect'
 
-
-// class BookShelf extends React.Component{
-// 	render(){
-// 		return ( 
-// 			     <div className="bookshelf">
-//                   <h2 className="bookshelf-title">{this.props.shelfName}</h2>
-//                   <div className="bookshelf-books">
-//                   	<BooksGrid bookList = {this.props.bookList} shelfName = {this.props.shelfName}/>
-//                   </div>
-//                 </div>
-// 		)
-// 	}
-// }
-
 class BooksGrid extends React.Component {
 	render() {
 		return (
@@ -26,13 +12,13 @@ class BooksGrid extends React.Component {
 					<li key = {item.id}>
 					<div className="book">
 					<div className="book-top">
-					<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url(" + item.imageLinks.thumbnail + ")" }}></div>
+					<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${item.imageLinks.thumbnail?item.imageLinks.thumbnail:'default'})` }}></div>
 						<div className="book-shelf-changer">
 							<BookMoveSelect bookObj = {item}/>
 						</div>
 					</div>
-					<div className="book-title">{item.title}</div>
-					<div className="book-authors">{item.authors}</div>
+					<div className="book-title">{item.title?item.title:'Unknown Title'}</div>
+					<div className="book-authors">{item.authors?item.authors:'Unknown Authors'}</div>
 					</div>
 					</li>		
 				))	
